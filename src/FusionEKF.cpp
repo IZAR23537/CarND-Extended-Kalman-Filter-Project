014576da3e30ea137deb1294e135271c875f1406
David Göndör <gondordavid@gmail.com>
2019-01-20 13:01:53 +0100

    Add files via upload

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -154,7 +154,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // TODO: Radar updates
 	
-	Tools tools;
+    Tools tools;
     ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
     ekf_.R_ = R_radar_;
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
@@ -163,7 +163,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   } else {
     // TODO: Laser updates
 	
-	ekf_.H_ = H_laser_;
+    ekf_.H_ = H_laser_;
     ekf_.R_ = R_laser_;
     ekf_.Update(measurement_pack.raw_measurements_);
 
